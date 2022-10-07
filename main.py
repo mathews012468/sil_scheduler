8ded9b809bd290420d390bd4c3259f4cd15a0eb7
Sil Demmer <Sil@martian-machine.lan>
2022-10-07 16:28:15 -0400

    Clean up
    * wrapped assigning roles to days into a function
    * filled out list current avaliable roles
    * deleted old code

--- main.py
+++ main.py
@@ -20,9 +20,16 @@ class Role:
 	def __init__(self, name, callTime=None, employee=None): 
 		self.name = name
 
+		#TODO: Add calltimes for all roles
 		#default values for calTime based on role:
 		callTimes = {
 			'lunch': datetime.time(hour=10, minute=30),
+			'brunch':datetime.time(hour=10, minute=30),
+			'bbar': datetime.time(hour=16, minute=30),
+			'vbar': datetime.time(hour=16, minute=30),
+			'middle': datetime.time(hour=18),
+			'back': datetime.time(hour=18),
+			'veranda': datetime.time(hour=16, minute=30),
 			'front': datetime.time(hour=16, minute=30),
 			'aux': datetime.time(hour=18),
 			'shermans': datetime.time(hour=16, minute=30)
@@ -32,6 +39,20 @@ class Role:
 			raise ValueError('Provide recognized role name or call time.')
 		self.employee = employee
 
+# a list of current roles at the restaurant
+roles = [
+	Role('lunch'),
+	Role('brunch'),
+	Role('bbar'),
+	Role('vbar'),
+	Role('middle'),
+	Role('back'),
+	Role('veranda'),
+	Role('front'),
+	Role('aux'),
+	Role('shermans')
+	]
+
 class Employee:
 	'''There are employees of the restaurant. an employee at Kiki's has a name,
 and an amount of shifts they can work in a week.'''
@@ -39,35 +60,6 @@ and an amount of shifts they can work in a week.'''
 		self.name = name
 		self.numberOfShifts = numberOfShifts # number of shifts they can work this week
 		self.avaliablity = avaliablity # a list of the employee's avalability for the week
-
-	# This requests idea is too far a reach while the core objects,
-	# Days, Roles, and Employees are not yet sorted out. Can be ignored for now
-	def requests(self):
-		''' An attempt to figure out how to store employee requests\n
-		using employee1's requests (see below) as an example'''
-		monday = self.avaliablity[0].replace(hour=16, minute=30) # avaliable from 16:30 on
-		tuesday = self.avaliablity[1] # open avaliablity on Tuesday
-		Wednesday = self.avaliablity[2].replace(hour=16, minute=30) # same as monday
-		#..
-		#..
-		saturday = self.avaliablity[5] # a timedelta object? how to get the duration of 
-		# 'avaliable between 10:30am-4:30pm'?
-
-#The idea is to have the callTime indepenant of a date at role creation, and ideally, once a role is 'assigned'-
-# to a Day object, be able to 'retrieve a concrete Date + Time' object useable in further calculations
-# e.g: endTime - callTime = hours worked
-
-front = Role(name='Front', callTime = datetime.time(hour=16))
-lunch = Role(name='Lunch', callTime = datetime.time(hour=10, minute=30))
-aux = Role(name='Aux', callTime = datetime.time(hour=18))
-shermans = Role(name='Shermans', callTime=datetime.time(hour=16, minute=30))
-bbar = Role(name='BBar',callTime=datetime.time(hour=16, minute=30))
-middle = Role(name='Middle', callTime=datetime.time(hour=18))
-back = Role(name='Back', callTime=datetime.time(hour=18))
-brunch = Role(name='Brunch', callTime=datetime.time(hour=10, minute=30))
-
-roles = [front, lunch, aux, shermans, bbar, middle, back, brunch]
-# A list of roles objects, ready to be tied to Day objects
 
 employee1 = Employee(name='Dr. DeSpencer', numberOfShifts=4)
 #Employee1's requests for the week are:
@@ -88,11 +80,13 @@ employee4 = Employee(name='Deckard Cain', numberOfShifts=5)
 
 employees = [employee1, employee2, employee3, employee4]
 # A list of Employee objects to be assigned to Roles throughout the workWeek.
+ 
 
+monday = Day(datetime.datetime(2022,10,3)) # this is Monday, Oct 3rd.
 
 
 def createWeek(day):
-	'''Create a 7 day week from the given start day.\n
+	'''Create a list of seven consecutive Day objects from the given start day.\n
 	arg = Day object.\n
 	returns: List'''
 
@@ -102,21 +96,26 @@ def createWeek(day):
 		week.append(nextDay)
 	return week
 
-monday = Day(datetime.datetime(2022,10,3)) # this is Monday, Oct 3rd.
+workWeek = createWeek(monday) # Seven day workWeek, starting from Monday, Oct 3rd.
 
-workWeek = createWeek(monday) # A list of seven consecutive Day objects.
-for day in workWeek:
-	day.roles = [Role('front'), Role('lunch'), Role('aux'), Role('shermans')]
+def assignRolesToDays(workWeek):
+	''' Assigns the list of roles to each day in the workWeek'''
+	for day in workWeek:
+		day.roles = roles
+	return workWeek
+
+weekAndRoles = assignRolesToDays(workWeek)
 
 def createSchedule(workWeek, employees):
+	'''assigns an employee to each role in each day of the week'''
 	for day in workWeek:
 		#for each role, assign an employee
 		for role in day.roles:
 			role.employee = random.choice(employees)
 
-createSchedule(workWeek, employees)
+createSchedule(weekAndRoles, employees)
 
-for day in workWeek:
+for day in weekAndRoles:
 	print(day.date)
 	for role in day.roles:
 		print(role.name)
@@ -134,38 +133,3 @@ def getPossibleRoles(roles, avaliablity):
 			e.g. [time1, time2, time3] means the employee is available from time-time2
 				and from time3 until the end of the day
 	"""
-
-
-
-# # Old code that the above code grew from.
-# # Question: Is this part of what 'version control' can be used for?
-# # to keep a document focused while previously written code remains accessible?
-
-# monday = datetime.datetime(2022,10,3) # this is Monday, Oct 3rd.
-
-# def weekOf(startDate):
-# 	'''Create a 7 day week from the given startDate.
-
-# 	arg = datetime.datetime object.
-
-# 	returns Week as a list object'''
-# 	week = []
-# 	for i in range(7):
-# 		nextDay = [startDate + datetime.timedelta(days=(i))]
-# 		week.append(nextDay)
-# 	return week
-
-# week = weekOf(monday) # The week of Monday, Oct 3rd.
-# #The idea here is to represent 'a week made up of days',
-# #to create a space for roles to be assinged to each day.
-
-# def scheduleTemplate(week,roles):
-# 	""" Appends a list of roles into each day of the week
-# 	returning a 'scheduleTemplate'"""
-# 	for i in range(len(week)):
-# 		week[i].append(roles)
-# 	return week
-
-# workWeek = scheduleTemplate(week, roles)
-# #This is the attempt at 'tieing' the roles onto each day of the week
-# #Though now that it's here- this is messy and not the desired outcome.
